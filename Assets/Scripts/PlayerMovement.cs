1aa2046c8499e7c204aef458931f84be0e1698f6
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2018-03-31 22:57:24 +0200

    add jump and trail

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour {
 
 	public float forwardForce = 2000f;
 	public float sidewaysForce = 500f;
+	public float jumpForce = 200f;
 
 	// Use this for initialization
 	void Start () {
@@ -15,13 +16,17 @@ public class PlayerMovement : MonoBehaviour {
 	void FixedUpdate () {
 		rb.AddForce(0, 0, this.forwardForce * Time.deltaTime);
 
-		if (Input.GetKey ("d")) {
+		if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
 			rb.AddForce(this.sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 		}
 
 
-		if (Input.GetKey ("a")) {
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
 			rb.AddForce(-1 * this.sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		if (Input.GetKey(KeyCode.Space) && this.rb.position.y > 1 && this.rb.position.y < 1.1) {
+			rb.AddForce(0, this.jumpForce * Time.deltaTime, 0, ForceMode.VelocityChange);
 		}
 
 		if (this.rb.position.y < -1f) {
